28bddfc5b55d059ff87134ef0c9aba445d2aba2c
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-02-07 08:56:49 -0700

    added courses

---
+++ app/views/courses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @course, :id, :name, :department, :code, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207043250) do
+ActiveRecord::Schema.define(version: 20160207154245) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.string   "department"
+    t.integer  "code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sections", force: true do |t|
     t.integer  "course_id"
