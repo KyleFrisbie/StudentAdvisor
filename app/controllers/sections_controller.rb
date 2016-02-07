66b92c5b9810b6ee8a4f091904cfb310534ba79d
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-02-06 22:12:11 -0700

    students and sections now relate via views

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -15,10 +15,12 @@ class SectionsController < ApplicationController
   # GET /sections/new
   def new
     @section = Section.new
+    @students = Student.all
   end
 
   # GET /sections/1/edit
   def edit
+    @students = Student.all
   end
 
   # POST /sections
@@ -69,6 +71,6 @@ class SectionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def section_params
-      params.require(:section).permit(:course_id, :professor_id, :crn, :building, :room, :start_time, :end_time, :start_date, :end_date, :days)
+      params.require(:section).permit(:course_id, :professor_id, :crn, :building, :room, :start_time, :end_time, :start_date, :end_date, :days, :student_ids => [])
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+students = Student.create([{first_name: 'Kyle', last_name: 'Frisbie', record_number: 0, major: 'Computer Science'},
+                           {first_name: 'C.J.', last_name: 'Anderson', record_number: 22, major: 'Football'},
+                           {first_name: 'Shaquil', last_name: 'Barrett', record_number: 48, major: 'Football'},
+                           {first_name: 'Sam', last_name: 'Brenner', record_number: 67, major: 'Football'},
+                           {first_name: 'BLAKE', last_name: 'COMEAU', record_number: 14, major: 'Hockey'},
+                           {first_name: 'MATT', last_name: 'DUCHENE', record_number: 9, major: 'Hockey'},
+                           {first_name: 'MIKHAIL', last_name: 'GRIGORENKO', record_number: 25, major: 'Hockey'},
+                           {first_name: 'Tyler', last_name: 'Anderson', record_number: 44, major: 'Baseball'},
+                           {first_name: 'Christian', last_name: 'Bergman', record_number: 36, major: 'Baseball'},
+                           {first_name: 'Chad', last_name: 'Bettis', record_number: 35, major: 'Baseball'},
+                           {first_name: 'Michael', last_name: 'Backens', record_number: 99, major: 'Rugby'},
+                           {first_name: 'Cody', last_name: 'Blake', record_number: 100, major: 'Rugby'},
+                           {first_name: 'Blake', last_name: 'Brown', record_number: 101, major: 'Rugby'}
+                          ])
+
+# sections = Section.create([{course_id: 0, professor_id: 0, crn: 31434, building: 'Science', room: '1011',
+#                             start_time: '2000-01-01T10:00:00.000Z', end_time: '2000-01-01T11:50:00.000Z',
+#                             start_date: 2016-02-07, end_date: 2016-02-08, days: 'MW'},
+#
+#                            {course_id: 0, professor_id: 0, crn: 2000, building: 'Science', room: '1011',
+#                             start_time: '2000-01-01T04:52:00.000Z', end_time: '2000-01-01T04:53:00.000Z',
+#                             start_date: 2016-02-07, end_date: 2016-02-08, days: 'MW'},
+#
+#                            {course_id: 0, professor_id: 0, crn: 2000, building: 'Science', room: '1011',
+#                             start_time: '2000-01-01T04:52:00.000Z', end_time: '2000-01-01T04:53:00.000Z',
+#                             start_date: 2016-02-07, end_date: 2016-02-08, days: 'MW'},
+#
+#                            {course_id: 0, professor_id: 0, crn: 2000, building: 'Science', room: '1011',
+#                             start_time: '2000-01-01T04:52:00.000Z', end_time: '2000-01-01T04:53:00.000Z',
+#                             start_date: 2016-02-07, end_date: 2016-02-08, days: 'MW'},
+#
+#                            {course_id: 0, professor_id: 0, crn: 2000, building: 'Science', room: '1011',
+#                             start_time: '2000-01-01T04:52:00.000Z', end_time: '2000-01-01T04:53:00.000Z',
+#                             start_date: 2016-02-07, end_date: 2016-02-08, days: 'MW'}
+#                           ])
